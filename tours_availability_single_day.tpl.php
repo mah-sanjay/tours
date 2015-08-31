896c1f79b7c7b71a49e204ba7348bd5007ed59da
Nicolò Caruso <nicolo@istos.it>
2015-08-31 15:33:10 +0200

    TOUR-40: Display settings for single day tour

--- tours_availability_single_day.tpl.php
+++ tours_availability_single_day.tpl.php
@@ -1,2 +1,9 @@
 <div class="tours-base-price">Starting from: <?php print $tours_availability_single_day['base_price']; ?></div>
-<?php print render($tours_availability_single_day['form']); ?>
+
+<?php if ($tours_availability_single_day['settings']['display_calendar']):
+        print render($tours_availability_single_day['form']);
+      endif; ?>
+
+<?php if ($tours_availability_single_day['settings']['enquire_about']): ?>
+	<div class="enquire-about"><a href="/enquiry-tour/">Enquire about this Tour</a></div>
+<?php endif; ?>
